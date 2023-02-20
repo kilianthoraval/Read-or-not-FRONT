1adfb037671214cc6f481472bba566bcdd35b7a3
Whitney Toniolo <whitney.toniolo@gmail.com>
2023-02-20 18:11:25 +0100

    création du module inscription en dynamique

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,48 +1,82 @@
-import React, { useState } from 'react';
-import './Signup.scss';
+import React from 'react';
+import { useState } from 'react';
+import './Signup.scss'
 
 function RegistrationForm() {
-  const [pseudo, setPseudo] = useState('');
-  const [password, setPassword] = useState('');
-  const [passwordConfirm, setPasswordConfirm] = useState('');
-  const [email, setEmail] = useState('');
-  const [emailConfirm, setEmailConfirm] = useState('');
+  const [formState, setFormState] = useState({
+    username: '',
+    password: '',
+    confirmPassword: '',
+    email: '',
+    confirmEmail: '',
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState((prevState) => ({ ...prevState, [name]: value }));
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Ajouter ici la logique pour traiter les données du formulaire
+    // Effectuer la soumission du formulaire ici (par exemple, envoyer les données au serveur)
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <label>
         Pseudo:
-        <input type="text" value={pseudo} onChange={(e) => setPseudo(e.target.value)} />
+        <input
+          type="text"
+          name="username"
+          value={formState.username}
+          onChange={handleChange}
+        />
       </label>
       <br />
       <label>
         Mot de passe:
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <input
+          type="password"
+          name="password"
+          value={formState.password}
+          onChange={handleChange}
+        />
       </label>
       <br />
       <label>
         Confirmation du mot de passe:
-        <input type="password" value={passwordConfirm} onChange={(e) => setPasswordConfirm(e.target.value)} />
+        <input
+          type="password"
+          name="confirmPassword"
+          value={formState.confirmPassword}
+          onChange={handleChange}
+        />
       </label>
       <br />
       <label>
         Adresse email:
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input
+          type="email"
+          name="email"
+          value={formState.email}
+          onChange={handleChange}
+        />
       </label>
       <br />
       <label>
         Confirmation de l'adresse email:
-        <input type="email" value={emailConfirm} onChange={(e) => setEmailConfirm(e.target.value)} />
+        <input
+          type="email"
+          name="confirmEmail"
+          value={formState.confirmEmail}
+          onChange={handleChange}
+        />
       </label>
       <br />
-      <input type="submit" value="S'inscrire" />
+      <button type="submit">S'inscrire</button>
     </form>
   );
 }
+
 
 export default RegistrationForm;

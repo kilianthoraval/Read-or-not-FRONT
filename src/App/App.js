2ecec40e59b23b52ca5b434e047bebc110459ed9
MickaelMallet <mickaelmallet7@gmail.com>
2023-02-23 15:36:12 +0100

    Authentification Axios Fetch, Whitney, jm, Mika

--- src/App/App.js
+++ src/App/App.js
@@ -4,12 +4,10 @@ import Navbar from '../components/navbar/Navbar';
 import Home from '../components/home/Home';
 import Signup from '../components/auth/Signup';
 import Footer from '../components/footer/Index';
-import LoginPage from '../components/auth/Login';
-
+import LoginForm from '../components/auth/Login';
 import MentionsLegales from '../components/Mentions/MentionsLegales';
 import ContactForm from '../components/contact/ContactForm';
 import About from '../components/About/About'
-
 import LibraryPage from '../components/library';
 import Loader from './Loader';
 import './loaderStyle.scss';
@@ -28,23 +26,6 @@ function App() {
     <Router>
       <div className="app-container">
 
-     
-        <Navbar />
-        <Routes>
-          <Route path="/" element = {<Home />} />
-          <Route path="/signup" element = {<Signup />} />
-          <Route path="/login" element = {<LoginPage />} />
-
-          <Route path="/mentionslegales" element = {<MentionsLegales />} />
-          <Route path="/contact" element = {<ContactForm />} />
-          <Route path="/about" element = {<About />} />
-
-          <Route path="/bibliotheque" element = {<LibraryPage />} />
-
-        </Routes>
-        <Footer />
-        
-
         {loading ? (
           <Loader /> // Afficher le loader si la page est en cours de chargement
         ) : (
@@ -53,7 +34,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/signup" element={<Signup />} />
-              <Route path="/login" element={<LoginPage />} />
+              <Route path="/login" element={<LoginForm />} />
               <Route path="/bibliotheque" element={<LibraryPage />} />
             </Routes>
             <Footer />
--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,41 +1,77 @@
 import React, { useState } from 'react';
-import './Login.scss'
+import { useLocation, Link } from 'react-router-dom';
+import axios from 'axios';
+import './Login.scss';
 
-function LoginPage() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+const LoginForm = () => {
+  // Utiliser useLocation pour rediriger vers la page précédente après l'authentification
+  const location = useLocation();
+  const { from } = location.state || { from: { pathname: '/' } };
 
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
-  };
+  // État local pour stocker les valeurs du formulaire
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
 
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
+  // État local pour stocker les messages d'erreur
+  const [errorMsg, setErrorMsg] = useState('');
 
-  const handleSubmit = (event) => {
+  // Fonction pour gérer la soumission du formulaire
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    // Logique de connexion à ajouter ici
+
+    // Appel API pour se connecter avec les informations du formulaire
+    try {
+      const response = await axios.post('http://kilianthoraval-server.eddi.cloud/auth/login', formData);
+      const { accessToken } = response.data;
+
+      // Stocker le jeton d'authentification dans le stockage local
+      localStorage.setItem('accessToken', accessToken);
+
+      // Rediriger vers la page précédente
+      window.location.replace(from.pathname);
+    } catch (error) {
+      // Gérer les erreurs d'authentification
+      if (error.response && error.response.status === 401) {
+        setErrorMsg('Adresse email ou mot de passe incorrect');
+      } else {
+        setErrorMsg('Une erreur est survenue, veuillez réessayer plus tard');
+      }
+    }
+  };
+
+  // Fonction pour mettre à jour les valeurs du formulaire en fonction de l'entrée utilisateur
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
   };
 
   return (
     <div className="login-container">
-      <h1 className="login-title">Connexion</h1>
-      <form onSubmit={handleSubmit} className="login-form">
-        <label>
-          Nom d'utilisateur :
-          <input type="text" value={username} onChange={handleUsernameChange} />
-        </label>
-        <br />
-        <label>
-          Mot de passe :
-          <input type="password" value={password} onChange={handlePasswordChange} />
-        </label>
-        <br />
-        <input type="submit" value="Se connecter" />
+      <form className="login-form" onSubmit={handleSubmit}>
+        <h2 className="login-title">Connexion</h2>
+        <div className="form-group">
+          <label htmlFor="email">Adresse email</label>
+          <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Mot de passe</label>
+          <input type="password" id="password" name="password" value={formData.password} onChange={handleChange} required />
+        </div>
+        <div className="form-group">
+          <input type="submit" value="Se connecter" />
+        </div>
+        {errorMsg && <div className="error-msg">{errorMsg}</div>}
+        <div className="signup-link">
+          Pas encore inscrit ? <Link to="/signup">Créer un compte</Link>
+        </div>
       </form>
     </div>
   );
-}
+};
 
-export default LoginPage;
+export default LoginForm;

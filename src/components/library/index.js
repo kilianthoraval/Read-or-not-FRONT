0da75b68797565f10dc63efa96e54c628100285d
jmlapernat <jmlapernat@gmail.com>
2023-03-01 14:54:06 +0100

    mise en place et tet slider library

--- src/components/library/index.js
+++ src/components/library/index.js
@@ -2,41 +2,68 @@
 
 import React, { useState, useEffect } from 'react';
 
+import AwesomeSlider from "react-awesome-slider";
+import "react-awesome-slider/dist/styles.css";
+
+
 
 import "./style.scss";
 
 
 function LibraryPage() {
-  const [books, setBooks] = useState([]);
- 
+  // const [books, setBooks] = useState([]);
+  const [data, setData] = useState([]);
 
   
   useEffect(() => {
-    const url = 'http://kilianthoraval-server.eddi.cloud:8080/books';
-    fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      setBooks(data);
-    })
-    .catch((error) => console.log(error));
+//     const url = 'http://kilianthoraval-server.eddi.cloud:8080/books';
+//      fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       setBooks(data);
+//     })
+//     .catch((error) => console.log(error));
+// }, []);
+const fetchData = async () => {
+  const response = await fetch("http://kilianthoraval-server.eddi.cloud:8080/books");
+  const result = await response.json();
+  setData(result);
+};
+
+fetchData();
 }, []);
+
+// return (
+//   <div>
+//     <h2>Liste des livres :</h2>
+    
+//     <ul>
+//       {books.map((book) => (
+//         <li key={book.id}>
+         
+//           {book.title} - {book.author} - {book.category} - {book.description} - {book.img}
+//         </li>
+//       ))}
+//     </ul>
+//   </div>
   
-
-
+// );
+// }
 return (
   <div>
-    <h2>Liste des livres :</h2>
-    <ul>
-      {books.map((book) => (
-        <li key={book.id}>
-          {book.title} - {book.author} - {book.category} - {book.description} - {book.img}
-        </li>
+    <h1>React Awesome Slider avec appel à une API</h1>
+    <AwesomeSlider>
+      {data.map((book) => (
+        <div key={book.id}>
+          <img src={book.img} alt={book.title} />
+          <h2>{book.title}</h2>
+          <p>{book.description}</p>
+        </div>
       ))}
-    </ul>
+    </AwesomeSlider>
   </div>
 );
-}
-
+};
 
 
 export default LibraryPage;
